b1e72aa1bb005904ff31a8eba32f526893a5a115
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-06 23:04:48 +0100

    Updated to fix an issue

--- mari/users/forms.py
+++ mari/users/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput)
-    password = forms.Charfield(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
