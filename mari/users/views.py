6c0d8087800b9091d319c9279fc4a4cf0f7f79c9
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-06 22:55:59 +0100

    Added initial login_view

--- mari/users/views.py
+++ mari/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import UserLoginForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data
+            # ...
+
+            return HttpResponseRedirect("/redirected-to-profile-dashboard")
+        
+    else:
+        form = UserLoginForm()
+    
+    return render(request, "login.html", {"form": form})
+
