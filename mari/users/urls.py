231eec483dea70f4dde398a9a557c2af77a04fc3
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-08 21:19:51 +0100

    Updated path code to fix errors

--- mari/users/urls.py
+++ mari/users/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
 urlpatterns = [
+    # Path to a login page with a login form
     path('login/', views.login_view, name='login'),
-    path('', include('django.contrib.auth.urls'))
+
+    # Path to a page to enter an email address to request password reset link
+    path('password-reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
+
+    # Path to a page to to confirm that email will be sent to user
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    # Path to a page with the password reset form to create a new password
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    # Path to a page confirming that a new password has been set
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
