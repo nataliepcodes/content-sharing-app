a85493083122aa0944269f411b3c04b7c7d18082
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-08 16:57:31 +0100

    Added new url path

--- mari/users/urls.py
+++ mari/users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('users/', include('django.contrib.auth.urls'))
 ]
